7bf2f882d68bff26111310e75d5c7b1e376065cd
pbokolo <dev.pbokolo@gmail.com>
2024-01-03 10:10:46 +0200

    Cleaning up the code

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const today = new Date();
 
 const timeLbl = document.querySelector(".text--time-main-weather");
 const tempLbl = document.querySelector(".text--temp");
+const windLbl = document.querySelector(".text--wind-speed");
+const humidityLbl = document.querySelector(".text--humidity");
 
 function displayTime() {
   const options = {
@@ -20,10 +22,26 @@ function handleCoords(position) {
 }
 
 async function handleForecastResult(result) {
+  //1-Parses the result in json
   const data = await result.json();
-  const { current } = data;
-  renderForecast(current);
-  console.log(current, data);
+  //2-Gets the hourly object
+  const hourly = data.hourly;
+  //3-Extract the array of temperatures, humidity and wind speed
+  const { temperature_2m, relative_humidity_2m, wind_speed_10m } = hourly;
+  //4-Computes sum of temperatures, humidities and wind speeds
+  let meanTemp = temperature_2m.reduce((acc, cur) => acc + cur, 0);
+  let meanHumidity = relative_humidity_2m.reduce((acc, cur) => acc + cur, 0);
+  let meanWindSpeed = wind_speed_10m.reduce((acc, cur) => acc + cur, 0);
+  //5-Then computes the mean value
+  meanTemp = meanTemp / temperature_2m.length;
+  meanHumidity = meanHumidity / relative_humidity_2m.length;
+  meanWindSpeed = meanWindSpeed / wind_speed_10m.length;
+  //6-Render forecast
+  renderForecast({
+    temp: meanTemp.toFixed(1),
+    humidity: meanHumidity.toFixed(1),
+    windSpeed: meanWindSpeed.toFixed(1),
+  });
 }
 
 async function getGeoCoords() {
@@ -46,36 +64,12 @@ async function getForecast({ latitude, longitude }) {
   }
 }
 
-/*interval
-: 
-900
-temperature_2m
-: 
-22.6
-time
-: 
-"2024-01-03T07:30"
-wind_speed_10m
-: 
-3.1*/
-
-/*interval
-: 
-"seconds"
-temperature_2m
-: 
-"°C"
-time
-: 
-"iso8601"
-wind_speed_10m
-: 
-"km/h"*/
-
-function renderForecast(current) {
-  tempLbl.textContent = `${current.temperature_2m}°C`;
+function renderForecast({ temp, windSpeed, humidity }) {
+  tempLbl.textContent = `${temp}°C`;
+  windLbl.textContent = `${windSpeed} km/h`;
+  humidityLbl.textContent = `${humidity} %`;
 }
 
 displayTime();
 
-getGeoCoords();
+// getGeoCoords();
